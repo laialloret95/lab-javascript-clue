8888742c3c8d4621793b7aa5b56b977feda2ac15
Laia Lloret <laialloret95@gmail.com>
2021-02-20 20:19:27 +0100

    add Rooms and Weapons collections

--- src/clue.js
+++ src/clue.js
@@ -61,10 +61,36 @@ const suspectsArray = [
 ];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+    "Dinning Room",
+    "Conservatory",
+    "Kitchen",
+    "Study",
+    "Library",
+    "Billiard Room",
+    "Lounge",
+    "Ballroom",
+    "Hall",
+    "spa",
+    "Living Room",
+    "Observatory",
+    "Theater",
+    "Guest House",
+    "Patio"  
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+    {name: "rope", weight: 10},
+    {name: "knife", weight: 8},
+    {name: "candlestick", weight: 2},
+    {name: "dumbbell" , weight: 30},
+    {name: "poison", weight: 2},
+    {name: "axe", weight: 15},
+    {name: "bat", weight: 13},
+    {name: "trophy", weight: 25},
+    {name: "pistol", weight: 20}
+];
 
 // ITERATION 2
 
